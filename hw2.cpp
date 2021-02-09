cf5eacaa3b5a0fd06c0ccbb9a4be908f94c0addc
Tsermaa444 <78241909+Tsermaa444@users.noreply.github.com>
2021-02-08 21:43:17 -0700

    Add files via upload

---
+++ hw2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n; //n=20
+    
+    for(int i = 1; i<=n ; i++){
+        cout<<i<<" ";
+    }
+
+    
+    return 0;
+}
